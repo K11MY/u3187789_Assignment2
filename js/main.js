0db9c27efa725995088af8066d8aa005010dc547
K11MY <kimpham99@protonmail.com>
2020-07-14 23:01:20 +1000

    added more comments into code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
+// grab toTop button
 const toTop = document.querySelector(".to-top");
-
+// when the user scroll add button 
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 100) {
     toTop.classList.add("active");
@@ -7,11 +8,10 @@ window.addEventListener("scroll", () => {
     toTop.classList.remove("active");
   }
 });
-
+// Smooth scroll javascript
 const scroll = new SmoothScroll(
   '.navbar a[href*="#"]',
-  '.to-top a[href*="#"]',
-  {
+  '.to-top a[href*="#"]', {
     speed: 800,
   }
 );
